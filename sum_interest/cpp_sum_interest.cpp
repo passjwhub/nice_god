a566d9f01719ff7c6f2d199adfd638b447582eb7
passjwhub <autocommsky@gmail.com>
2019-10-12 09:18:01 +0800

    sum information

--- sum_interest/cpp_sum_interest.cpp
+++ sum_interest/cpp_sum_interest.cpp
@@ -4,11 +4,13 @@ using namespace std;
 int main()
 {
     double  money,years,rate,sum;
+    cout<<"please input principal, years, rate, sum, seclude them with space"<<endl;
     cin>>money>>years>>rate;
     while(money>0)
     {
         sum=money*pow((1+rate),years);
-        cout<<sum<<endl;
+        cout<<"total need give back:"<<sum<<endl;
+        cout<<"please input principal, years, rate, sum, quit with input 0"<<endl;
         cin>>money>>years>>rate;
     }
     return 0;
